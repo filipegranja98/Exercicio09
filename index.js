78e05040e8e395d8b97acc077783c5dc87a0d398
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-05-17 00:11:34 -0300

    Primeiro commit da atividade
    Consegui realizar o que o professor propôs na atividade, porém falta estilizar com o CSS.

---
+++ index.js
@@ -0,0 +1,110 @@
+const lista = document.getElementById("lista");
+const inputDescricao = document.getElementById("inputDescricao");
+const btAdd = document.getElementById("btAdd");
+
+const taskUrl = "https://parseapi.back4app.com/classes/Task";
+const headers = {
+  "X-Parse-Application-Id": "jrxekyyy7IV5c3jD7soaxnR0kH7Xerwy54lCMujo",
+  "X-Parse-REST-API-Key": "7PFo378gfkDmlHMDDqrGlhYFojFYTawW75ZhGCNu",
+};
+
+const renderizaLista = (lista, tarefas) => {
+  lista.innerHTML = "";
+  tarefas.forEach((tarefa) => {
+    const itemText = document.createTextNode(
+      `${tarefa.Description}`
+    );
+    const buttonDelete = document.createElement("button");
+    buttonDelete.id = "x";
+    buttonDelete.innerHTML = "X";
+    buttonDelete.onclick = () => deleteTask(tarefa.objectId);
+    const buttonUpdate = document.createElement("input");
+    buttonUpdate.type = 'checkbox';
+    buttonUpdate.checked = tarefa.done;
+    buttonUpdate.onclick = () => updateTask(tarefa);
+    const listItem = document.createElement("li");
+    if(tarefa.done == true){
+        listItem.style.textDecoration = "line-through";
+    }
+    listItem.appendChild(itemText);
+    listItem.appendChild(buttonUpdate);
+    listItem.appendChild(buttonDelete);
+    lista.appendChild(listItem);
+  });
+};
+
+const getTasks = () => {
+  fetch(taskUrl, { headers: headers })
+    .then((res) => res.json())
+    .then((data) => {
+      renderizaLista(lista, data.results);
+    });
+};
+
+const handleBtAddClick = () => {
+  const description = inputDescricao.value;
+  if (!description) {
+    alert("É necessário digitar uma descrição!");
+    return;
+  }
+  btAdd.disabled = true;
+  fetch(taskUrl, {
+    method: "POST",
+    headers: {
+      ...headers,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ Description: description }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      getTasks();
+      btAdd.disabled = false;
+      inputDescricao.value = "";
+      inputDescricao.focus();
+      console.log("data", data);
+    })
+    .catch((err) => {
+      btAdd.disabled = false;
+      console.log(err);
+    });
+};
+
+const deleteTask = (id) => {
+  fetch(`${taskUrl}/${id}`, {
+    method: "DELETE",
+    headers: headers,
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      getTasks();
+      console.log("data", data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const updateTask = (task) => {
+  fetch(`${taskUrl}/${task.objectId}`, {
+    method: "PUT",
+    headers: headers,
+    headers: {
+      ...headers,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ done: !task.done }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      getTasks();
+      console.log("data", data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+getTasks();
+
+btAdd.onclick = handleBtAddClick;
